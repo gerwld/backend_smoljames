5bd1d9827f303ca6061d9ef4abe17538dd3c2cf0
P Jaworski <pjaworski.dev@gmail.com>
2025-02-08 00:07:20 +0100

    upd

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -2,8 +2,36 @@ const express = require("express");
 const app = express();
 const PORT = 8383;
 
+
+// Middleware
+app.use(express.json());
+
+
+// Website endpoints
+
+app.get("/", (req,res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.send("123")
+})
+
+app.get("/dashboard", (req,res) => {
+    res.send("dashboard route")
+})
+
+// API endpoints
+
+app.get("/api/data", (req,res) => {
+    res.status(201).json({message: "actual data"})
+})
+
+app.post('/api/data', (req,res) => {
+    const {name} = req.body;
+    if(!name?.length) return res.status(400).json({message: "All fields are required"});
+
+    res.status(201).json({message: `Successfully added ${name}`})
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server has started on ${8383}`);
-    
 });
-
