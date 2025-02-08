6fd09a9a1bc9f6d99843159ec1f6c1fdcb947321
P Jaworski <pjaworski.dev@gmail.com>
2025-02-08 01:24:56 +0100

    added public, etc

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require('path');
 
+// Serves the HTML file from the /public directory
+// Tells express to serve all files from the public folder as static assets / file.
+app.use(express.static(path.join(__dirname, '../public')));
 
-
-
-
-
-
+// Serving up the HTML file from the /public directory
+app.get('/', (req, res) => {
+   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
    console.log(`Server started on port ${PORT}`);
